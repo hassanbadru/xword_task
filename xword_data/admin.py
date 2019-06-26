5e4d2b3139c4e6561ba6ccea4d629177ad6541e3
Hassan B <hassan.badru@gmail.com>
2019-06-26 10:12:58 -0400

    register models w/ admin + initial migration

--- xword_data/admin.py
+++ xword_data/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from xword_data.models import Puzzle, Entry, Clue
 
-# Register your models here.
+admin.site.register(Puzzle)
+admin.site.register(Entry)
+admin.site.register(Clue)
