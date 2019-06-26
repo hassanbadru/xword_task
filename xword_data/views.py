c8e972d87cea590a079ec4bf4ac9227563ca7974
Hassan B <hassan.badru@gmail.com>
2019-06-26 11:04:06 -0400

    setup skeleton for drill view + template to fetch new random clue, initialize new game session on login, created new entry form and post method to receive user guesses on drill view (current redirects to same page), some bootstrap styling

--- xword_data/views.py
+++ xword_data/views.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
+from django.urls import reverse
+
+from .models import Puzzle, Entry, Clue
+from .forms import EntryForm
 
 # view for user to start new drill
 class LogInView(TemplateView):
@@ -9,6 +15,14 @@ class LogInView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(LogInView, self).get_context_data(**kwargs)
+
+        # initialize new session
+        self.request.session['total'] = 0
+        self.request.session['correct'] = 0
+
+        if self.request.session.has_key('clue_id'):
+            del self.request.session['clue_id']
+
         return context
 
 # view to present random clue with information about entry
@@ -17,7 +31,23 @@ class DrillView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(DrillView, self).get_context_data(**kwargs)
+
+        # fetch random clue
+        random_clue = Clue.objects.order_by("?").first()
+        clue_id = random_clue.id
+        print(random_clue)
+
+        self.request.session['clue_id'] = clue_id
+        self.request.session['total'] += 1
+
+        context['entry_form'] = EntryForm()
+        context['random_clue'] = random_clue
         return context
+
+    def post(self, request, *args, **kwargs):
+        entry_form = EntryForm(request.POST or None)
+        print(entry_form.is_valid())
+        return HttpResponseRedirect(reverse('drill'))
 
 # view to congratulates the user on their success
 class AnswerView(TemplateView):
